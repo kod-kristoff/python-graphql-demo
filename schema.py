d1a1b95a7f5155f280086b1558ca89fae645d4b7
Kristoffer Andersson <kode.kristoff@gmail.com>
2018-11-08 09:07:44 +0100

    schema work

--- schema.py
+++ schema.py
@@ -1,5 +1,11 @@
 import graphene
 
+
+class User(graphene.ObjectType):
+    id = graphene.ID()
+    name = graphene.String()
+
+    
 class Query(graphene.ObjectType):
   hello = graphene.String(name=graphene.String(default_value="World"))
 
